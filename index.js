336c76b93d7fb9832055dfb15a4f083536cf8b9a
Muhammed Basheer <basheerkomassery@gmail.com>
2023-09-28 17:37:06 +0530

    Edited files to make the code more simple with the help of react and JSX. compare the last commit with this changes to get better understanding.

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// DOM maninpulation using js file.
+// DOM maninpulation using react.
 
 
 // Select the div element with 'app' id
 const app = document.getElementById('app');
 
-// create a new H1 element
+// do the all previous jobs in just one line.
 
-const header = document.createElement('h1');
-
-// create a new text node for the H1 element
-
-const headerContent = document.createTextNode(
-    'Develop. Preview. Ship.'
-);
-
-// append the text to the H1 element
-
-header.appendChild(headerContent);
-
-// place the H1 element inside the div
-
-app.appendChild(header);
+ReactDOM.render(<h1>Develop. Preview. Ship. 🚀</h1>, app);
